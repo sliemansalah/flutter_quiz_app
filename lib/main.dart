167846ce60f0af7b12b53dd7910712f9160a12a9
slieman <slieman2001@hotmail.com>
2020-03-20 15:01:18 +0200

    update ..

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,30 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-          title: Text(
-        'My First App',
-      )),
-      body: Text('This is my default text'),
-      Text('Second text') // not true.
-    ));
+            appBar: AppBar(
+                title: Text(
+              'My First App',
+            )),
+            body: Column(children: [
+              Text('The question!'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: null,
+              ),
+            ])));
   }
 }
